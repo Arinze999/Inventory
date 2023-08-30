787329a7103d91e0542ea4bc4ab0e0189342af20
Arinze Akanaga <akanagapyt@gmail.com>
2023-08-30 22:11:30 +0100

    modified:   assets/script/createaccountscript.js 	modified:   assets/script/login.js 	modified:   assets/styles/homestyle.css 	modified:   assets/styles/userloginstyle.css 	modified:   index.html 	modified:   userLogin.html 	modified:   assets/script/createaccountscript.js 	modified:   assets/styles/crateAccountstyle.css 	modified:   createAccount.html

--- assets/script/createaccountscript.js
+++ assets/script/createaccountscript.js
@@ -29,28 +29,29 @@ email.addEventListener("change", () => {
 // to loop through and get each already existing userDetails within the user
 function myFunction(userDetails) {
   for (let i = 0; i < users.length; i++) {
-    const existingUsers = users[i];
-    console.log(existingUsers)
+    var existingUsers = users[i];
+    console.log(existingUsers);
 
-    //compare properties of new userDeatils with existing userDetails
-    let alreadyExisting = true;
-    let alreadyExistingUsername = true;
-    let alreadyExistingStorename = true;
-    const existingUsersCheck1 = existingUsers.Username;
-    const existingUsersCheck2 = existingUsers.Email;
-    const existingUsersCheck3 = existingUsers.Storename;
-    const userDetailscheck1 = userDetails.Username;
-    const userDetailscheck2 = userDetails.Email;
-    const userDetailscheck3 = userDetails.Storename;
-    
- 
-      if (userDetailscheck1 == existingUsersCheck1) {
-        console.log("same")
-      }
-      else {
-        console.log("notsame")
-      }
-    
+    if (
+      existingUsers.Username == userDetails.Username &&
+      existingUsers.Email == userDetails.Email
+    ) {
+      console.log("this user exists");
+    } else if (
+      existingUsers.Username == userDetails.Username &&
+      existingUsers.Email !== userDetails.Email
+    ) {
+      console.log("this username has been taken");
+    } else if (
+      existingUsers.Username !== userDetails.Username &&
+      existingUsers.Email == userDetails.Email
+    ) {
+      console.log("this Email is already registered");
+    } else if (existingUsers.storename == userDetails.Storename) {
+      console.log("this store has been taken");
+    } else {
+      console.log("valid user!");
+    }
   }
 }
 
@@ -84,50 +85,42 @@ createButton.addEventListener("click", () => {
     alert("FILL EMAIL PROPERLY");
   } else if (userDetails.Confirmpassword != userDetails.PassWord) {
     alert("CROSSCHECK PASSWORD AND CONFIRM!");
-  }
-  else {
-    usersCounter++;
-    myFunction(userDetails);
-    users.push(userDetails);
-
-    // users.forEach((item) => {
-    //   // if (item.Username === item.Username) {
-    //   //   console.log("same username")
-    //   // }
-    // });
-    if (
-      !(
-        userDetails.Fullname == "" ||
-        userDetails.Username == "" ||
-        userDetails.Storename == "" ||
-        userDetails.Email == "" ||
-        userDetails.PassWord == "" ||
-        userDetails.Confirmpassword == ""
-      )
-    ) {
-      alert("ACOUNT CREATED!");
+  } else {
+    if (myFunction(userDetails)) {
+      alert("already");
     } else {
+      usersCounter++;
+      users.push(userDetails);
+      if (
+        !(
+          userDetails.Fullname == "" ||
+          userDetails.Username == "" ||
+          userDetails.Storename == "" ||
+          userDetails.Email == "" ||
+          userDetails.PassWord == "" ||
+          userDetails.Confirmpassword == ""
+        )
+      ) {
+        alert("ACOUNT CREATED!");
+      } else {
+      }
+      console.log(users);
+      // localStorage.setItem("Users", users);
+
+      // let registeredUsers = localStorage.getItem("Users");
+
+      // console.log(registeredUsers);
+
+      // localStorage.clear();
     }
   }
-  console.log(users);
 });
 
-export { users };
+// this code is not complete
 
 // var example = "exxx";
 
 // export {example};
-
-
-
-
-
-
-
-
-
-
-
 
 //function to check if an object already exists in the array
 // function isDuplicateuserDetails(userDetails) {
@@ -148,4 +141,50 @@ export { users };
 //     }
 //   }
 //   return false;
+// }
+
+//   //compare properties of new userDeatils with existing userDetails
+//   // var alreadyExistingUser = true;
+//   // var alreadyExistingUsername = true;
+//   // var alreadyExistingUserEmail = true;
+//   // var alreadyExistingStorename = true;
+//   const enableCheck1 = existingUsers.id;
+//   const enableCheck2 = userDetails.id;
+//   const existingUsersCheck1 = existingUsers.Username;
+//   const existingUsersCheck2 = existingUsers.Email;
+//   const existingUsersCheck3 = existingUsers.Storename;
+//   const userDetailscheck1 = userDetails.Username;
+//   const userDetailscheck2 = userDetails.Email;
+//   const userDetailscheck3 = userDetails.Storename;
+
+//   if (enableCheck1 == enableCheck2) {
+//     return false;
+//   }
+//   else {
+//     if (
+//       (userDetailscheck1 == existingUsersCheck1) &&
+//       (userDetailscheck2 == existingUsersCheck2)
+//     ) {
+//       console.log("user already exists");
+//       return true;
+//     } else if (
+//       (userDetailscheck1 !== existingUsersCheck1) &&
+//       (userDetailscheck2 == existingUsersCheck2)
+//     ) {
+//       console.log("this email is already registered to an account");
+//       return true;
+//     } else if (
+//       (userDetailscheck1 == existingUsersCheck1) &&
+//       (userDetailscheck2 !== existingUsersCheck2)
+//     ) {
+//       console.log("this username is not available");
+//       return true;
+//     } else if (userDetailscheck3 == existingUsersCheck3) {
+//       console.log("this store has already been taken");
+//       return true;
+//     } else {
+//       console.log("new user is valid");
+//       return false;
+//     }
+//   }
 // }
--- assets/script/login.js
+++ assets/script/login.js
@@ -1,5 +1,23 @@
 // import {example} from "./createaccountscript.js";
 // console.log(example);
 
-import {users} from "./createaccountscript.js";
-console.log(users);
+// import {users} from "./createaccountscript.js";
+// console.log(users);
+alert("DEMO USERNAME = JOHN , PASSWORD = 0000");
+
+const userName = document.getElementById("user");
+const passWord = document.getElementById("pass-word");
+const loginButton = document.getElementById("login-btn");
+
+const allowedUser = "john";
+const allowedPassword = "0000";
+var portalTo = "accountUser.html";
+
+loginButton.addEventListener("click" , () => {
+    if (userName.value.toLowerCase() === allowedUser.toLowerCase() && passWord.value === allowedPassword) {
+        window.location.href = portalTo;
+    }
+    else {
+        alert("DEMO USERNAME = JOHN , PASSWORD = 0000");
+    }
+});
